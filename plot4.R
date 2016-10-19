adafe9f765134f0b27a18a8db1d0815132cf30df
prakharm1 <prakharmaini@gmail.com>
2016-10-19 12:59:29 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,17 @@
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# subsetting SCC with coal values
+coalMatches  <- grepl("coal", SCC$Short.Name, ignore.case=TRUE)
+subsetSCC <- SCC[coalMatches, ]
+
+# merging dataframes
+NEISCC <- merge(NEI, subsetSCC, by="SCC")
+
+# summing emission data per year
+totalEmissions <- tapply(NEISCC$Emissions, NEISCC$year, sum)
+
+# plotting
+barplot(totalEmissions, xlab = "Year", ylab = "Total Emission (ton)", 
+        main = "Total Emission from coal sources")
